c5a85ce3429b19039527612a7a5fb75f268867a7
joaogomessilva <joaogomes.silva@uol.com.br>
2023-05-09 15:14:48 -0300

    Projeto criado

---
+++ aulaextra/src/main/java/com/aulaextra/devsuperior/aulaextra/AulaextraApplication.java
@@ -0,0 +1,13 @@
+package com.aulaextra.devsuperior.aulaextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaextraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaextraApplication.class, args);
+	}
+
+}
